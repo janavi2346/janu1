f05e60af7866e1f0a6cb18542feccb5d225f28fa
jahnavi <gjahnavireddy1@gmail.com>
2026-02-25 11:10:36 +0530

    lab3calculator.java

--- project/src/main/java/project/calculator.java
+++ project/src/main/java/project/calculator.java
@@ -5,9 +5,8 @@ public class calculator {
 	public static void main(String[] args) {
 		System.out.println(26);
 		System.out.println(34);
-		System.out.println(33);
-		System.out.println(25);
-		System.out.println(26);
+		System.out.println("janu");
+		
 	}
 
 }
